262874ae4c47f1fb0735a57c4dea560d4c833125
Vivek-Singh-Bisht <vivekbisht602@gmail.com	password = Ankuvivo@1>
2025-02-21 01:15:17 +0530

    setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'production', // or 'development'
-  entry: './src/index.ts',
+  mode: "development", // or "production"
+  entry: "./src/index.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  }
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: "cheap-source-map", // ✅ Fix: Avoids 'eval()'
 };
